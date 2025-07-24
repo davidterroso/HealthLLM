9cd32fd227f839af286df90fb558f4ff869cf8c3
DavidTerroso <davidterroso@live.com.pt>
2025-07-24 12:03:26 +0100

    merging branches and updating files

--- data_handling/db_searching.py
+++ data_handling/db_searching.py
@@ -1,19 +1,70 @@
 import json
+from langchain import hub
+from langchain_huggingface.llms import HuggingFacePipeline
 from langchain_community.vectorstores import FAISS
+from langchain_core.documents import Document
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
 from langchain_huggingface import HuggingFaceEmbeddings
+from transformers import pipeline, AutoModelForSeq2SeqLM, AutoTokenizer
+from typing import List
 
 with open("config.json", "r") as f:
         config = json.load(f)
 
-def search_by_query(query: str):
+def search_by_query(query: str) -> List[Document]:
+        """
+        Searches the given query in the VectorDB, 
+        embedding it previously
+
+        Args:
+                query (str): query used to search 
+                        the document 
+        
+        Returns:
+                results (List[Document]): list of 
+                        documents retrieved
+        """
         embedding_function = HuggingFaceEmbeddings(model_name=config["local_embedding_model"])
-        vector_store = FAISS.load_local("../local_embeddings/faiss_pmc", embeddings=embedding_function, allow_dangerous_deserialization=True)
+        # Dangerous deserialization is on because the data is local
+        vector_store = FAISS.load_local("../local_embeddings/faiss_pmc", 
+                                        embeddings=embedding_function, 
+                                        allow_dangerous_deserialization=True)
 
         results = vector_store.similarity_search(query=query, k=3)
 
-        for i, doc in enumerate(results):
-                print(f"\n--- Result {i+1} ---\n")
-                print(doc.page_content)
+        return results
 
+def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+
+def answer_questions(query: str):
+        """
+        """
+        tokenizer = AutoTokenizer.from_pretrained(config["local_llm"])
+        model = AutoModelForSeq2SeqLM.from_pretrained(config["local_llm"])
+
+        embedding_function = HuggingFaceEmbeddings(model_name=config["local_embedding_model"])
+        # Dangerous deserialization is on because the data is local
+        vector_store = FAISS.load_local("../local_embeddings/faiss_pmc", 
+                                        embeddings=embedding_function, 
+                                        allow_dangerous_deserialization=True)
+
+        pipe = pipeline("text2text-generation", model=model, tokenizer=tokenizer)
+        llm = HuggingFacePipeline(pipeline=pipe)
+
+        prompt = hub.pull("rlm/rag-prompt")
+
+        qa_chain = (
+                {       
+                        "context": vector_store.as_retriever() | format_docs,
+                        "question": RunnablePassthrough(),
+                }
+                | prompt
+                | llm
+                | StrOutputParser()
+        )
+        qa_chain.invoke(query)
+        
 query = "foot shape differences in diabetic patients"
-search_by_query(query=query)
+answer_questions(query=query)
